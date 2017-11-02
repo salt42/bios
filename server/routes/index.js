41f358dce26d2d6b7a5715d403d7cbc55f0953f1
salt <boluuuu@gmail.com>
2017-11-02 01:46:44 +0100

    need npm install!

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('layout', {
+    res.render('index', {
         title: "login"
     });
 });
