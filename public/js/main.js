8251784ebdf1e58e9062e97030acada382a61601
Fry <Fry.LMU@gmx.de>
2017-11-04 15:06:15 +0100

    function test works

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,23 @@
 (function() {
     "use strict";
 
+    let startUpLog = true;
     const global = {
         UIModules: {}
     };
     let Modules = {},
         UIModules = {};
 
+    function sLog(value1, value2 = null, value3 = null){
+        if (startUpLog){
+            if (value3 !== null)
+                console.log(value1, value2, value3);
+            else if (value2 !== null)
+                console.log(value1, value2);
+            else console.log(value1);
+        }
+
+    }
 
     function createNewUI(name, $parent) {
         //neues ele in parent
@@ -19,6 +30,7 @@
             console.error("Module name '"+ moduleName +"' taken");
         }
         Modules[moduleName] = initMethod;
+        sLog("module attached: ", moduleName)
     };
     //defines a UI module
     window.defineUI = function(uiName, initMethod) {
@@ -28,9 +40,10 @@
         UIModules[uiName] = initMethod;
     };
     window.onload = function() {
-        console.log("init system");
+        sLog("init system");
         //init modules
         for(let module in Modules) {
+            sLog("init module: ", module);
             if (!Modules.hasOwnProperty(module)) continue;
             let context = {};
             Modules[module].call(context, global);
@@ -39,12 +52,12 @@
 
         // -> document ready
         // //init UI modules
-        console.log(UIModules);
+        sLog("uiModules: ", UIModules);
         for(let module in UIModules) {
             if (!UIModules.hasOwnProperty(module)) continue;
-            console.log("search uiModule: ", module);
+            sLog("search uiModule: ", module);
             $(module).each(function(index, ele) {
-                console.log("init module", ele);
+                sLog("init uiModule", ele);
                 let context = {};
                 UIModules[module].call(context, global, $(ele) );
                 // global.UIModules[module] = context;
