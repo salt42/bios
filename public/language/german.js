7bbebbbc5faca4002750b2e5dff5f71cc54e70b3
salt <boluuuu@gmail.com>
2017-11-11 15:37:48 +0100

    rest

---
+++ public/language/german.js
@@ -0,0 +1,14 @@
+window.trans = new Map();
+
+let helper;
+
+//Enums
+helper = "GENDER";
+trans.set(helper + "_" + "0", "männlich");
+trans.set(helper + "_" + "1", "weiblich");
+trans.set(helper + "_" + "2", "unbekannt");
+
+// translations default in lowercase
+trans.set("gender", "geschlecht");
+trans.set("first name", "Vorname");
+trans.set("died on", "verstorben am");
