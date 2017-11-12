371f4e646ec1d31504fef9d82c1e4b5df6066a34
Fry <Fry.LMU@gmx.de>
2017-11-12 18:32:57 +0100

    added regions for overwiew

--- server/database.js
+++ server/database.js
@@ -11,16 +11,16 @@ console.log(__dirname + config.dbFile);
 var DB = new Database(__dirname + config.dbFile, {});
 
 module.exports = {
-    SELECT: 'SELECT ',
-    FROM: ' FROM ',
-    WHERE: ' WHERE ',
-    ORDER: ' ORDER BY ',
+    _SELECT: 'SELECT ',
+    _FROM: ' FROM ',
+    _WHERE: ' WHERE ',
+    _ORDER: ' ORDER BY ',
 
     testResults(){
         return this.liveSearchArticle("e");
     },
 
-    // live search
+    /*region liveSearch*/
     liveSearchAll(query){
         let dbResults = {};
         dbResults.animals = {};
@@ -35,15 +35,16 @@ module.exports = {
     },
 
     liveSearchOwner(query){
-        //todo rebuild query to get a sorted string
+        //todo rebuild query to get a sorted result
         let search = {
             query: "%"+query+"%"
         },
             select = 'owner.owner_id, owner.first_name, owner.first_name_2, owner.name, owner.name_2, owner.address, owner.Zip, ' +
                      'owner.City, owner.address_2, owner.Zip_2, owner.City_2',
             from = 'owner',
-            where = '(owner_id || salutation || first_name || first_name_2 || name || name_2 || address || Zip || City || ' +
-                    'address_2 || Zip_2 || City_2 || telephone_1 || telephone_2 || telephone_3 || telephone_4 || e_mail || www)',
+            where = '(owner_id || salutation || first_name || name || address || Zip || City || ' +
+                    'first_name_2 || name_2 || address_2 || Zip_2 || City_2 || ' +
+                    'telephone_1 || telephone_2 || telephone_3 || telephone_4 || e_mail || www)',
             Compare = ' ' + 'like @query',
             orderStr = 'name ASC, \n CASE WHEN name = \''+query+'\' THEN 1 ' +
                               'WHEN name like \''+query+'%\' Then 2 ' +
@@ -53,7 +54,7 @@ module.exports = {
                               'WHEN name_2 like \'%'+query+'%\' THEN 6 ' +
                               'WHEN first_name_2 like \'%'+query+'%\' THEN 7 ' +
                             'END ASC',
-            statement = this.SELECT + select + this.FROM + from + this.WHERE + where + Compare + this.ORDER + orderStr
+            statement = this._SELECT + select + this._FROM + from + this._WHERE + where + Compare + this._ORDER + orderStr
         ;
 
         // console.log('####### \n QUERY: ', query);
@@ -67,9 +68,10 @@ module.exports = {
             },
             select = 'animal.id, animal.species_id, animal.race_id, animal.name, animal.birthday, animal.gender, animal.color_description, animal.died, animal.died_on',
             from = 'animal',
-            where = '(owner_id || species_id || race_id || chip || tattoo || name || birthday || color_description || died_on)',
+            where = '(owner_id || ' +    // deprecated when owner-animal-hash implemented!!
+                    'species_id || race_id || chip || tattoo || name || birthday || color_description || died_on)',
             Compare = ' ' + 'like @query',
-            statement = this.SELECT + select + this.FROM + from + this.WHERE + where + Compare
+            statement = this._SELECT + select + this._FROM + from + this._WHERE + where + Compare
         ;
 
         // console.log('####### \n QUERY: ', query);
@@ -83,9 +85,10 @@ module.exports = {
             },
             select = 'articles.id, articles.article_number, articles.name, articles.got, articles.vendor, articles.article_target',
             from = 'articles',
-            where = '(article_number || name || volume || got || vendor || charge_number || invoice_id || tax_rate || sub_unit_1 || sub_unit_2 || sub_unit_3 || article_target)',
+            where = '(article_number || name || volume || got || vendor || charge_number || invoice_id || tax_rate || ' +
+                    'sub_unit_1 || sub_unit_2 || sub_unit_3 || article_target)',
             Compare = ' ' + 'like @query',
-            statement = this.SELECT + select + this.FROM + from + this.WHERE + where + Compare
+            statement = this._SELECT + select + this._FROM + from + this._WHERE + where + Compare
         ;
 
         // console.log('####### \n QUERY: ', query);
@@ -93,8 +96,9 @@ module.exports = {
         let row = DB.prepare(statement).all(search);
         return row;
     },
+    /*endregion*/
 
-    // get lists
+    /*region getLists*/
     getAllLists(){
         let result = {};
         result.listSpecies = this.getList("species");
@@ -114,8 +118,9 @@ module.exports = {
     getList(table){
         return DB.prepare('select * from ' + table).all();
     },
+    /*endregion*/
 
-
+    /*region search*/
     searchOwners(query){
         //todo rebuild query to get a sorted string
         let search = {
@@ -132,7 +137,7 @@ module.exports = {
             'when name like \'%'+query+'\' then 3 ' +
             'when name like \'%'+query+'%\' then 4 ' +
             'end) ASC',
-            statement = this.SELECT + select + this.FROM + from + this.WHERE + where + Compare + this.ORDER + orderStr
+            statement = this._SELECT + select + this._FROM + from + this._WHERE + where + Compare + this._ORDER + orderStr
         ;
 
         // console.log('####### \n QUERY: ', query);
@@ -140,7 +145,9 @@ module.exports = {
         let row = DB.prepare(statement).all(search);
         return row;
     },
+    /*endregion*/
 
+    /*region helper*/
     // helper
     /**
      * Sorts out the dead animals
@@ -176,6 +183,9 @@ module.exports = {
         }
         return res;
     },
+    /*endregion*/
+
+    /*region example*/
 
     exampleJoin(){
         let testa = "SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate" +
@@ -184,6 +194,7 @@ module.exports = {
             "INNER JOIN species ON animal.species_id=species.id"+
             "INNER JOIN race ON animal.race_id=race.id"
     },
+    /*endregion*/
 };
 
 function exitHandler(options, err) {
@@ -199,128 +210,7 @@ process.on('SIGINT', exitHandler);
 process.on('uncaughtException', exitHandler);
 
 
-var SQL = {
-    create (tablename = null) {
-        this._table = "";
-        this._selectColumns = "";
-        this._where = "";
-        this._operator = "";
-        this._searchQuery = "";
-        this._limit = "";
-        this._order = "";
-        this._statement = "";
-        if (tablename !== null)
-            this._table = tablename;
-        return this;
-    },
-    setTable (tablename){
-        this._table = tablename;
-        this._columns = "";
-        return this;
-    },
-    setSELECTColumns (columns){
-        if (this._table === "") console.log("specify table before!!!");
-        else if (columns === "" || columns === null) { }
-        else if (columns === "*") {
-            this._selectColumns = "*";
-        }
-        else {
-            this._selectColumns = columns;
-        }
-        return this;
-    },
-    addSELECTColumns (columnName){
-        if (this._table === "") console.log("specify table before!!!");
-        else if (columnName === "" || columnName === null || columnName === "*") { }
-        else if (columnName.indexOf(".") > -1) {
-            if (this._selectColumns === "") this._selectColumns = columnName;
-            else this._selectColumns += ", " + columnName;
-        }
-        else {
-            if (this._selectColumns === "") this._selectColumns = this._table + "." + columnName;
-            else this._selectColumns += ", " + this._table + "." + columnName;
-        }
-        return this;
-    },
-    setWHEREColumns (whereColumns) {
-        if (this._table === "") console.log("specify table before!!!");
-        else if (whereColumns === "" || whereColumns === null) { }
-        else if (whereColumns === "*") {
-            this._where = "";
-        }
-        else {
-            this._where = whereColumns;
-        }
-        return this;
-    },
-    addWHEREColumn (whereColumns) {
-        if (this._table === "") console.log("specify table before!!!");
-        else if (whereColumns === "" || whereColumns === null || whereColumns === "*") { }
-        else {
-            if (this._where === "") this._where = whereColumns;
-            else this._where += " || " + whereColumns;
-        }
-        return this;
-    },
-    setOperator (operator) {
-        if (this._table === "") console.log("specify table before!!!");
-        else this._operator = operator;
-        return this;
-    },
-    setSearchQuery (query) {
-        if (this._table === "") console.log("specify table before!!!");
-        else this._searchQuery = query;
-        return this;
-    },
-    setLIMIT (limit) {
-        if (this._table === "") console.log("specify table before!!!");
-        else this._limit = limit;
-        return this;
-    },
-    setORDERString (orderString) {
-        if (this._table === "") console.log("specify table before!!!");
-        else {
-            if (orderString.toLowerCase().indexOf("order by") > -1)
-                this._order = orderString;
-            else this._order = 'ORDER BY ' + orderString
-        }
-        return this;
-    },
-    getSQLStatement () {
-        if ( this._table === "" || (this._where !== "" && this._searchQuery ==="")){
-            let msg = "SQLStatement set up is incorrect";
-            console.log (msg);
-            return msg;
-        } // query+ where, table,
-        this._statement = "SELECT ";
-        this._statement += (this._selectColumns === "") ? "* " : this._selectColumns.trim() + " ";
-        this._statement += 'FROM ' + this._table.trim() + " ";
-        if (this._where !== ""){
-            this._statement += 'WHERE (' + this._where.trim() + ') ';
-            this._statement += (this._operator === "")? '= ' : this._operator.trim() + " ";
-            if (typeof this._searchQuery === "string") {
-                if (this._searchQuery.trim().indexOf("@") > 2) {
-                    this._statement += '"' + this._searchQuery.trim() + '" ';
-                }
-                else {
-                    this._statement += this._searchQuery + ' ';
-                }
-            }
-            else {
-                this._statement += this._searchQuery + ' ';
-            }
-            // fix null-in-field-error
-            this._statement += 'or (' + this._where.trim() + ') IS NULL ';
-            //is null
-        }
-        this._statement += (this._order === "") ? "" : this._order.trim() + " ";
-        this._statement += (this._limit === "") ? "" : "LIMIT " + this._limit + " ";
-
-        return this._statement;
-    }
-};
-
-
+/*region textPrepare*/
 //================================================= TABLES =============================================================
 //======================================================================================================================
 /*
@@ -416,3 +306,4 @@ var SQL = {
 `article_target`	        INTEGER DEFAULT 1                  .   articles.article_target
 );
  */
+/*endregion*/
