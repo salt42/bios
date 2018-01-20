14ad3fb034ea6f5d03c4b278da421aa6d71d7ece
Fry <Fry.LMU@gmx.de>
2018-01-20 10:30:17 +0100

    added db_addObject examples

--- src/server/modules/search.js
+++ src/server/modules/search.js
@@ -14,79 +14,6 @@ let errorcodes = [
 ];
 /* endregion */
 
-// hookIn.createRoute("/search", function(router) {
-//     router.get('/all/:query', function(req, res) {
-//         let dbResults = DB.liveSearchAll(req.params.query);
-//
-//         let result = {
-//             query: req.params.query,
-//             owners: dbResults.owner,
-//             animals: dbResults.animals.alive,
-//             deadAnimals: dbResults.animals.dead,
-//             articles: dbResults.articles,
-//         };
-//
-//
-//         res.json(result);
-//     });
-//     router.get('/user', function(req, res) {
-//         let userList = DB.getUserList();
-//         let userListActive = [];
-//         let userListInactive = [];
-//
-//         for(let i = 0; i < userList.length; i++){
-//             if (userList[i].present === 0){
-//                 userListInactive.push(userList[i]);
-//             } else {
-//                 userListActive.push(userList[i]);
-//             }
-//         }
-//
-//         let result = {
-//             query: req.params.query,
-//             users: userList,
-//             usersActive: userListActive,
-//             usersInactive: userListInactive,
-//         };
-//
-//         res.json(result);
-//     });
-//     router.get('/list/:query', function(req, res) {
-//         let result = {};
-//         if (req.params.query === "species"){
-//             result.list = DB.getSpeciesList();
-//         }
-//         if(req.params.query === "userRoles"){
-//             result.list = DB.getUserRolesList();
-//         }
-//         else {
-//             result = DB.getAllLists();
-//         }
-//
-//         res.json(result);
-//     });
-//     router.get('/owners/:query', function(req, res) {
-//         let result = DB.searchOwners(req.params.query);
-//
-//         res.json(result);
-//     });
-//     router.get('/owner/:query', function(req, res) {
-//         let result = DB.searchOwnerByID(req.params.query);
-//
-//         res.json(result);
-//     });
-//     router.get('/animals/:query', function(req, res) {
-//         let result = DB.searchAnimals(req.params.query);
-//
-//         res.json(result);
-//     });
-//     router.get('/animal/:query', function(req, res) {
-//         let result = DB.searchAnimalByID(req.params.query);
-//
-//         res.json(result);
-//     });
-// });
-
 function cleanUpDoubleEntries(results){
     let ids = [],
         result = [];
@@ -142,7 +69,6 @@ function limitResults(results, count = 8){
     }
     return limitedResults;
 }
-
 
 /* region live search */
 hookIn.db_addMethod("liveSearchAll", function(DB) {
@@ -362,3 +288,77 @@ hookIn.db_addObject("getOwners", function (DB) {
     };
 });
 /* endregion */
+
+/* router conf.. deprecatet/in folder routes */
+// hookIn.createRoute("/search", function(router) {
+//     router.get('/all/:query', function(req, res) {
+//         let dbResults = DB.liveSearchAll(req.params.query);
+//
+//         let result = {
+//             query: req.params.query,
+//             owners: dbResults.owner,
+//             animals: dbResults.animals.alive,
+//             deadAnimals: dbResults.animals.dead,
+//             articles: dbResults.articles,
+//         };
+//
+//
+//         res.json(result);
+//     });
+//     router.get('/user', function(req, res) {
+//         let userList = DB.getUserList();
+//         let userListActive = [];
+//         let userListInactive = [];
+//
+//         for(let i = 0; i < userList.length; i++){
+//             if (userList[i].present === 0){
+//                 userListInactive.push(userList[i]);
+//             } else {
+//                 userListActive.push(userList[i]);
+//             }
+//         }
+//
+//         let result = {
+//             query: req.params.query,
+//             users: userList,
+//             usersActive: userListActive,
+//             usersInactive: userListInactive,
+//         };
+//
+//         res.json(result);
+//     });
+//     router.get('/list/:query', function(req, res) {
+//         let result = {};
+//         if (req.params.query === "species"){
+//             result.list = DB.getSpeciesList();
+//         }
+//         if(req.params.query === "userRoles"){
+//             result.list = DB.getUserRolesList();
+//         }
+//         else {
+//             result = DB.getAllLists();
+//         }
+//
+//         res.json(result);
+//     });
+//     router.get('/owners/:query', function(req, res) {
+//         let result = DB.searchOwners(req.params.query);
+//
+//         res.json(result);
+//     });
+//     router.get('/owner/:query', function(req, res) {
+//         let result = DB.searchOwnerByID(req.params.query);
+//
+//         res.json(result);
+//     });
+//     router.get('/animals/:query', function(req, res) {
+//         let result = DB.searchAnimals(req.params.query);
+//
+//         res.json(result);
+//     });
+//     router.get('/animal/:query', function(req, res) {
+//         let result = DB.searchAnimalByID(req.params.query);
+//
+//         res.json(result);
+//     });
+// });
