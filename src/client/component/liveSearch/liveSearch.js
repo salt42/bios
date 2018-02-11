43983c8b6b9d6c75debebc3f57c62b4cb8da877e
Fry <Fry.LMU@gmx.de>
2018-02-11 23:44:45 +0100

    changed live search

--- src/client/component/liveSearch/liveSearch.js
+++ src/client/component/liveSearch/liveSearch.js
@@ -1,184 +1,80 @@
-defineComp("live-search", function(bios, template){
+defineComp("live-search", function(bios, template) {
     "use strict";
+
 
     let $element = this.$ele;
 
-    if (window.startUpLogLevel >= 2) console.log("start search UI module");
-    let $input = $('<input />')
-        .attr("id", "fixed-header-drawer-exp")
-        .addClass("mdl-textfield__input")
-        .attr("type", "text")
-        .attr("name", "search")
-        .attr("placeholder", bios.trans.late("liveSearch placeholder") )
-        .appendTo($element);
+    bios.rxLiveSearch = {};
+    bios.rxLiveSearch.upStream = new Rx.ReplaySubject();
 
-    let $liveResults = $('<ul></ul>')
-        .addClass("liveResults")
-        .appendTo($element);
+    this.onLoad = function () {
+        console.log("loaded live-search COMP");
+        console.log("load search");
+        let $subComp = $('live-search-result').getComponent();
+        let $input = $('input', $element);
+        
+        let searchQuery = "";
 
-    let searchQuery = "";
-    let resultSet;
+        function updateResults(data){
+            $subComp.updateResults(data);
+        }
 
-    function updateResults(data){
-        for (let property in data) {
-            if (!data.hasOwnProperty(property)) continue;
-            if (data[property].length === 0 || property === "query") continue;
-            let $li = $('<li></li>');
-            let $ul = $('<ul></ul>');
-            let $fragment = $(document.createDocumentFragment());
-            let entryClass = "live-search";
-            // add entries
-            if (data.hasOwnProperty(property)) {
-                // console.log('##', property);
-                switch (property){
-                    case "owners":
-                        $li = $('<li class="owner-group group"></li>')
-                            .html(bios.trans.language("owner"));
-                        for (let i = 0; i < data[property].length; i++) {
-                            let a = data[property][i];
-                            let htmlText = " " + a.first_name + " " + a.name + ", " + a.address;
-                            $('<li></li>')
-                                .addClass(entryClass + " fa fa-user")
-                                .attr("type", 'owner')
-                                .attr("owner-id", a.id)
-                                .html(htmlText)
-                                .appendTo($fragment)
-                                .show();
-                        }
-                        // console.log("here " + property, data[property]);
-                        break;
-                    case "animals":
-                        $li = $('<li class="animals-group group"></li>')
-                            .html(bios.trans.language("animals"));
-                        for (let i = 0; i < data[property].length; i++) {
-                            let a = data[property][i];
-                            let htmlText = " " + a.name + ", " + trans(denumS(a.species_id));
-                            $('<li data-imgInfo = "' + a.species_id + '"></li>')
-                                .addClass(entryClass + " fa fa-paw")
-                                .attr("type", 'animal')
-                                .attr("animal-id", a.id)
-                                .html(htmlText)
-                                .appendTo($fragment)
-                                .show();
-                        }
-                        // console.log("here " + property, data[property]);
-                        break;
-                    case "deadAnimals":
-                        $li = $('<li class="dead-animals-group group"></li>')
-                            .html(bios.trans.language("dead animals"));
-                        for (let i = 0; i < data[property].length; i++) {
-                            let a = data[property][i];
-                            let htmlText = " " + a.name + ", " + trans(denumS(a.species_id)) + ", " + a.died_on;
-                            $('<li data-imgInfo = "' + a.species_id + '"></li>')
-                                .addClass(entryClass + " animal-dead" + " fa fa-circle")
-                                .attr("type", 'animal')
-                                .attr("animal-id", a.id)
-                                .html(htmlText)
-                                .appendTo($fragment)
-                                .show();
-                        }
-                        // console.log("here " + property, data[property]);
-                        break;
-                    case "articles":
-                        $li = $('<li class="articles-group group"></li>')
-                            .html(bios.trans.language("articles"));
-                        for (let i = 0; i < data[property].length; i++) {
-                            let a = data[property][i];
-                            let htmlText = " " + a.name;
-                            $('<li></li>')
-                                .addClass(entryClass + " fa fa-archive")
-                                .attr("type", 'article')
-                                .attr("article-id", a.id)
-                                .html(htmlText)
-                                .appendTo($fragment)
-                                .show();
-                        }
-                        break;
-                    default:
-                }
+        /* region search bar behavior */
+        $input.on("keyup", function(e) {
+            // $liveResults.empty()
+            //     .removeClass("hidden");
+            searchQuery = $input.val();
+            console.log('key up', e.keyCode);
+            switch (e.keyCode) {
+                case 16: // = shift
+                case 20: // = caps lock
+                case 18: // = alt
+                    break; // do nothing
+                case 13: // = enter
+                    // @todo action on enter
+                    break;
+                default:
+                    if (!(searchQuery === "")){
+                        // add wildcard to search
+                        if (!(searchQuery.slice(-1) === "*"))
+                            searchQuery += "*";
+                        // console.log("query: ", searchQuery);
+                        bios.search.liveSearch(searchQuery, function(data) {
+                            // console.log(data);
+                            if (data.query !== searchQuery) {
+                                console.log("returned query is wrong");
+                                return;
+                            }
+                            updateResults(data);
+                        }, "short");
+                    }
             }
-            $ul.append($fragment)
-                .appendTo($li);
-            $liveResults.append($li);
-        }
-        highlight(data["query"]);
-    }
-
-    function highlight(query) {
-        let $srcElements = $(".live-search");
-        for (let i = 0; i < $srcElements.length; i++){
-            let text = $srcElements[i];
-            let sQuery = query.substr(0,query.length-1);
-            let regex = new RegExp(sQuery, 'gi');
-            let response = text.innerText.replace(regex, function(str) {
-                return "<span class='highlight'>" + str + "</span>"
-            });
-            text.innerHTML = response
-
-        }
-    }
-
-    function denumS(value){
-        return bios.trans.decode.species(value);
-    }
-
-    $liveResults.on("click", function(e) {
-        let $target = $(e.target),
-            type = $target.attr("type"),
-            id = $target.attr(type + '-id')
-        ;
-
-        if (!$target.hasClass("live-search")) return;
-
-        bios.ems.ems_LiveSearch ({
-            type: type,
-            id: id
         });
-        $input.val("");
-        $liveResults.not("hidden").addClass("hidden");
+        /*endregion*/
+        bios.rxLiveSearch.upStream.subscribe(function(upstream){
+            console.log('get', upstream);
+            console.log($('live-search-result-item', upstream));
+        });
 
-    });
-
-    /* region search bar behavior */
-    $input.on("keyup", function(e) {
-        $liveResults.empty()
-            .removeClass("hidden");
-        searchQuery = $input.val();
-        // console.log(e.keyCode);
-        switch (e.keyCode) {
-            case 16: // = shift
-            case 20: // = caps lock
-            case 18: // = alt
-                break; // do nothing
-            case 13: // = enter
-                // @todo save last view
-                bios.loadComponent("live-searchresult", "mainSection", {
-                    query: searchQuery,
-                    resultSet: resultSet,
-                });
-                $liveResults.not("hidden").addClass("hidden");
-                break;
-            default:
-                if (!(searchQuery === "")){
-                    // add wildcard to search
-                    if (!(searchQuery.slice(-1) === "*"))
-                        searchQuery += "*";
-                    // console.log("query: ", searchQuery);
-                    bios.search.liveSearch(searchQuery, function(data) {
-                        // console.log(data);
-                        if (data.query !== searchQuery) {
-                            console.log("returned query is wrong");
-                            return;
-                        }
-                        resultSet = data;
-                        updateResults(data);
-                    }, "short");
-                }
-        }
-    });
-    /*endregion*/
+        // $liveResults.on("click", function(e) {
+        //     let $target = $(e.target),
+        //         type = $target.attr("type"),
+        //         id = $target.attr(type + '-id')
+        //     ;
+        //
+        //     if (!$target.hasClass("live-search")) return;
+        //
+        //     bios.ems.ems_LiveSearch ({
+        //         type: type,
+        //         id: id
+        //     });
+        //     $input.val("");
+        //     $liveResults.not("hidden").addClass("hidden");
+        //
+        // });
+    };
 }
-// ,{
-//     templatePath: "/component/liveSearch/liveSearch.html"
-// }
+,{
+    templatePath: "/component/liveSearch/liveSearch.html"
+}
 );
--- src/client/component/liveSearch/liveSearchResult/liveSearchResult.js
+++ src/client/component/liveSearch/liveSearchResult/liveSearchResult.js
@@ -1,19 +1,137 @@
 /**
  * Created by fry on 14.11.2017.
  */
-defineComp("live-search-result", function(bios, $element, args) {
-    $("<h1>Live Search Result</h1>")
-        .appendTo($element);
+defineComp("live-search-result", function(bios, template, args) {
 
+    // console.log("loading live-search-result COMP");
 
-    bios.initAllUI($element);
+    let $element = this.$ele;
 
-    bios.loadComponent("owners", "mainSection", args);
-    bios.loadComponent("animals", "mainSection", args);
+    this.onLoad = function () {
+        // console.log("loaded live-search-result COMP");
+    };
 
+    this.updateResults = function (data) {
+        $element.removeClass("hidden");
+        for (let property in data) {
+            if (!data.hasOwnProperty(property)) continue;
+            if (data[property].length === 0 || property === "query") continue;
+            let type;
+            // add entries
+            switch (property){
+                case "owners":
+                    type = "owner";
+                    $('li.group-' + type).removeClass("hidden");
+                    $('span', 'li.group-' + type).html(bios.trans.late("live search result " + type));
 
-    this.onDiscard = function() {};
-});
+                    $('ul.items', 'li.group-' + type)
+                        .empty()
+                        .appendTemplate(".template-live-search-result-item", data.owners, function (template, value) {
+                        $('li', template)
+                            .addClass("fa-user")
+                            .attr("type", type)
+                            .attr(type + "-id", value.id)
+                            .html(value.first_name + " " + value.name + ", " + value.address)
+                        ;
+                    });
+                    break;
+                case "animals":
+                    type = "animal";
+                    $('li.group-' + type).removeClass("hidden");
+                    $('span', 'li.group-' + type).html(bios.trans.late("live search result " + type));
+
+                    $('ul.items', 'li.group-' + type)
+                        .empty()
+                        .appendTemplate(".template-live-search-result-item", data.animals, function (fragment, value) {
+                        $('li', fragment)
+                            .addClass("fa-paw")
+                            .attr("type", type)
+                            .attr(type + "-id", value.id)
+                            .html(value.name + ", " + trans(denumS(value.species_id)))
+                        ;
+                    });
+                    break;
+                case "deadAnimals":
+                    type = "animal-d";
+                    $('li.group-' + type).removeClass("hidden");
+                    $('span', 'li.group-' + type).html(bios.trans.late("live search result " + type));
+
+                    $('ul.items', 'li.group-animal-d')
+                        .empty()
+                        .appendTemplate(".template-live-search-result-item", data.deadAnimals, function (fragment, value) {
+                        $('li', fragment)
+                            .addClass("fa-circle")
+                            .attr("type", type)
+                            .attr(type + "-id", value.id)
+                            .html(value.name + ", " + trans(denumS(value.species_id)) + ", " + value.died_on)
+                        ;
+                    });
+                    break;
+                case "articles":
+                    type = "article";
+                    $('li.group-' + type).removeClass("hidden");
+                    $('span', 'li.group-' + type).html(bios.trans.late("live search result " + type));
+
+                    $('ul.items', 'li.group-' + type)
+                        .empty()
+                        .appendTemplate(".template-live-search-result-item", data.articles, function (fragment, value) {
+                        $('li', fragment)
+                            .addClass("fa-archive")
+                            .attr("type", type)
+                            .attr(type + "-id", value.id)
+                            .html(value.name)
+                        ;
+                    });
+                    break;
+            }
+        }
+        highlight(data.query);
+        bios.rxLiveSearch.upStream.next($element);
+
+    }
+    function highlight(query) {
+        let $srcElements = $(".live-search-result-item");
+        for (let i = 0; i < $srcElements.length; i++){
+            let text = $srcElements[i];
+            let sQuery = query.substr(0,query.length-1);
+            let regex = new RegExp(sQuery, 'gi');
+            let response = text.innerText.replace(regex, function(str) {
+                return "<span class='highlight'>" + str + "</span>"
+            });
+            text.innerHTML = response
+
+        }
+    }
+
+    function denumS(value){
+        return bios.trans.decode.species(value);
+    }
+
+    this.getResult = function (){
+        return $element;
+    };
+    //
+    // $liveResults.on("click", function(e) {
+    //     let $target = $(e.target),
+    //         type = $target.attr("type"),
+    //         id = $target.attr(type + '-id')
+    //     ;
+    //
+    //     if (!$target.hasClass("live-search")) return;
+    //
+    //     bios.ems.ems_LiveSearch ({
+    //         type: type,
+    //         id: id
+    //     });
+    //     $input.val("");
+    //     $liveResults.not("hidden").addClass("hidden");
+    //
+    // });
+    }
+    ,{
+        templatePath: "/component/liveSearch/liveSearchResult/liveSearchResult.html"
+    }
+);
 
 
 // <ul class="demo-list-icon mdl-list">
