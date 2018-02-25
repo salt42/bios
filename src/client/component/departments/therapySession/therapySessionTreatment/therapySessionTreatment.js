7b135a7c1c25d65ea73814d3a5be6b90fa063dd7
Fry <Fry.LMU@gmx.de>
2018-02-25 11:41:00 +0100

    moved Treatment view to js added prepend Template function to jquery

--- src/client/component/departments/therapySession/therapySessionTreatment/therapySessionTreatment.js
+++ src/client/component/departments/therapySession/therapySessionTreatment/therapySessionTreatment.js
@@ -7,6 +7,18 @@ defineComp("therapy-session-treatment",  function (bios, template, args) {
     let $element = this.$ele;
 
     this.onLoad = function(){
+        let i = 0;
+        // past treats
+        $('.treatments').prependTemplate(".template-treatment",[{},{}], function (fragment, value) {
+            //insert data
+            $('p', fragment).text("old " + i++);
+        });
+        // actual new treat
+        $('.treatments').prependTemplate(".template-treatment",[{}], function (fragment, value) {
+            //insert data
+            $(fragment).addClass("new");
+            $('p', fragment).text("new");
+        });
     }
 
 }, {
